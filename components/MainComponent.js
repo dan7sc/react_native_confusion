6de093a00967cfb358c9ed159d3cae738c6070ff
daniel sc <su7cheng@gmail.com>
2019-09-17 17:20:59 -0300

    Navigation Part 2.

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { View, Platform } from 'react-native';
+import Home from './HomeComponent';
+import { View, Platform, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer, SafeAreaView } from 'react-navigation';
 
 
@@ -29,7 +31,46 @@ const MenuNavigator = createStackNavigator(
     }
 );
 
-const AppContainer = createAppContainer(MenuNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: Home
+        }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512da8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerLabel: 'Home'
+            }
+        },
+        Menu: {
+            screen: MenuNavigator,
+            navigationOptions: {
+                drawerLabel: 'Menu '
+            }
+        }
+    },
+    {
+        drawerBackgroundColor: '#d1c4e9'
+    }
+);
+
+const AppContainer = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import DISHES from '../shared/dishes.js';
+import DISHES from '../shared/dishes';
 
 
 class Menu extends Component {
