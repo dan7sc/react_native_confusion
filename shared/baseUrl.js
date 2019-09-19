9532f12393395eb3478d48b47de2696d0c9b7570
daniel sc <su7cheng@gmail.com>
2019-09-19 15:02:35 -0300

    Activity Indicator.

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,5 +1,7 @@
-const hostname = 'localhost';
+// const hostname = 'localhost';
 // const hostname = your_private_ip; // Tip: stop firewall
+const hostname = '100.112.112.58'
 const port = '3001';
 
-export const baseUrl = 'http://' + hostname + ':' + port + '/';
+// export const baseUrl = 'http://' + hostname + ':' + port + '/';
+export const baseUrl = 'https://dan7sc.github.io/serve-json/';
